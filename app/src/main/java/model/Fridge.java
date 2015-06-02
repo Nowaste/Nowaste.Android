7eed13ee397d83a209a57dd3d6bda920e215cd6d
RGiskard-Reventlov <florian.garnier30@gmail.com>
2015-06-02 19:00:06 +0200

    Update model architecture

---
+++ app/src/main/java/model/Fridge.java
@@ -0,0 +1,9 @@
+package model;
+
+import com.raizlabs.android.dbflow.annotation.Table;
+
+import data.FoodDatabase;
+
+@Table(databaseName = FoodDatabase.NAME)
+public class Fridge {
+}
---
+++ app/src/main/java/model/User.java
@@ -0,0 +1,10 @@
+package model;
+
+import com.raizlabs.android.dbflow.annotation.Table;
+
+import data.FoodDatabase;
+
+@Table(databaseName = FoodDatabase.NAME)
+public class User {
+    
+}
