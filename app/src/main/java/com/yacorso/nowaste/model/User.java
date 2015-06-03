66a420d40b9831767c5b4225ae9c7ef3823b3484
RGiskard-Reventlov <florian.garnier30@gmail.com>
2015-06-03 17:10:48 +0200

    Create User model

--- app/src/main/java/com/yacorso/nowaste/model/User.java
+++ app/src/main/java/com/yacorso/nowaste/model/User.java
@@ -14,13 +14,43 @@ public class User extends BaseCacheableModel {
     @PrimaryKey(autoincrement = true)
     protected int id;
     @Column
-    protected String name;
+    protected String firstName;
+
+    @Column
+    protected String lastName;
+
+    @Column
+    protected String salt;
+
+    @Column
+    protected String email;
+
+    @Column
+    protected String token;
+
+    @Column
+    protected boolean enabled;
 
     public User () { }
 
     public long getId() { return id; }
     public void setId(int id) { this.id = id; }
+    
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getSalt() { return salt; }
+    public void setSalt(String salt) { this.salt = salt; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getToken() { return token; }
+    public void setToken(String token) { this.token = token; }
+
+    public boolean isEnabled() { return enabled; }
+    public void setEnabled(boolean enabled) { this.enabled = enabled; }
 }
