ff4aef7f24df76f6605f0fcfee344bed6ecd65cf
Quentin Bontemps <q.bontemps@gmail.com>
2015-06-26 15:54:26 +0200

    add copyright header

--- app/src/main/java/com/yacorso/nowaste/services/FridgeService.java
+++ app/src/main/java/com/yacorso/nowaste/services/FridgeService.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2015.
+ *
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <q.bontemps@gmail> , <reventlov@tuta.io> and <marjorie.debote@free.com> wrote this file.
+ *  As long as you retain this notice you can do whatever you want with this stuff.
+ *  If we meet some day, and you think this stuff is worth it, you can buy me a beer in return.
+ *
+ * No Waste team
+ *
+ */
+
 package com.yacorso.nowaste.services;
 
 import com.yacorso.nowaste.NowasteApplication;
