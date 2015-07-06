f5bf61bc21c18b4d3683589207a93d096411cc69
MarjoDebote <Lowi_ena@hotmail.fr>
2015-07-06 18:14:25 +0200

    First unit test to try the button that add food

--- app/src/test/java/com/yacorso/nowaste/ApplicationTest.java
+++ app/src/test/java/com/yacorso/nowaste/ApplicationTest.java
@@ -12,11 +12,16 @@
 
 package com.yacorso.nowaste;
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.widget.Button;
 
 import com.yacorso.nowaste.views.activities.DrawerActivity;
+import com.yacorso.nowaste.views.fragments.AddFoodFragment;
+import com.yacorso.nowaste.views.fragments.BaseFragment;
+import com.yacorso.nowaste.views.fragments.FoodListFragment;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -24,6 +29,8 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+
+import butterknife.ButterKnife;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNotNull;
@@ -49,6 +56,50 @@ public class ApplicationTest {
 
     @Test
     public void clickingLogin_shouldStartLoginActivity() {
+        Intent expectedIntent = new Intent(activity, FragmentActivity.class);
+        assertThat(shadowOf(activity).getNextStartedActivity()).isEqualTo(expectedIntent);
+    }
+//    @Test
+//    public void clickingButton_shouldChangeResultsViewText() throws Exception {
+//        Activity activity = Robolectric.setupActivity(MyActivity.class);
+//
+//        Button pressMeButton = (Button) activity.findViewById(R.id.press_me_button);
+//        TextView results = (TextView) activity.findViewById(R.id.results_text_view);
+//
+//        pressMeButton.performClick();
+//        String resultsText = results.getText().toString();
+//        assertThat(resultsText, equalTo("Testing Android Rocks!"));
+//    }
+    @Test
+    public void clickingMike_shouldOpenBoxAddFood() throws Exception {
+        setup();
+
+        /**
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.welcome_activity);
+
+            final View button = findViewById(R.id.login);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+                }
+            });
+        }
+
+         @Test
+         public void clickingLogin_shouldStartLoginActivity() {
+             WelcomeActivity activity = Robolectric.setupActivity(WelcomeActivity.class);
+             activity.findViewById(R.id.login).performClick();
+
+             Intent expectedIntent = new Intent(activity, WelcomeActivity.class);
+             assertThat(shadowOf(activity).getNextStartedActivity()).isEqualTo(expectedIntent);
+         }
+    **/
+        //ButterKnife mFabButton = (ButterKnife) ButterKnife.findViewById(R.id.btnAddFood);
+
+        activity.findViewById(R.id.btnAddFood).performClick();
         Intent expectedIntent = new Intent(activity, FragmentActivity.class);
         assertThat(shadowOf(activity).getNextStartedActivity()).isEqualTo(expectedIntent);
     }
