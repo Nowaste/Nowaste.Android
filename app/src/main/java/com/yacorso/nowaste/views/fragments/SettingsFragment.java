117c5001137f1154eea7c82bb7da76ec4469914a
MarjoDebote <Lowi_ena@hotmail.fr>
2015-07-13 20:15:55 +0200

    Start of settings page, but spinner doesn't set his list

--- app/src/main/java/com/yacorso/nowaste/views/fragments/SettingsFragment.java
+++ app/src/main/java/com/yacorso/nowaste/views/fragments/SettingsFragment.java
@@ -12,14 +12,55 @@
 
 package com.yacorso.nowaste.views.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import com.yacorso.nowaste.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by quentin on 05/07/15.
  */
 public class SettingsFragment extends BaseFragment {
 
+    private Spinner mySpinner;
+
     public SettingsFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.fragment_settings, container, false);
+        mRootView = super.onCreateView(inflater, container, savedInstanceState);
+
+        setSpinnerContent(layout);
+
+        return mRootView;
+    }
+
+    private void setSpinnerContent(View view){
+        mySpinner = (Spinner) view.findViewById(R.id.spinner_notifications);
+
+        List<String> list = new ArrayList<String>();
+        list.add("1 jour");
+        list.add("2 jour");
+        list.add("3 jour");
+        list.add("5 jour");
+        list.add("1 semaine");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>
+                (getActivity(), android.R.layout.simple_spinner_item,list);
+        dataAdapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_item);
+        mySpinner.setAdapter(dataAdapter);
     }
 
     public static SettingsFragment newInstance(){
